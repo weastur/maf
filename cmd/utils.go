2552638f0bd3899cd612272e58b286dad0a71d28
Pavel Sapezhka <me@weastur.com>
2025-03-21 14:25:07 +0200

    cmd: create client for leader only

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,11 +1,17 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
+	"github.com/spf13/cobra"
 	"github.com/weastur/maf/pkg/config"
 	serverAPIClient "github.com/weastur/maf/pkg/server/client"
+	"github.com/weastur/maf/pkg/server/worker/fiber"
 )
+
+var errLeaderAddrNotFound = errors.New("leader API address not found")
 
 const (
 	defaultHTTPReadTimeout             = 5 * time.Second
@@ -20,16 +26,44 @@ type ServerAPIClient interface {
 	RaftKVDelete(key string) error
 }
 
-func getServerAPIClient() ServerAPIClient {
+func clientTLSConfig() *serverAPIClient.TLSConfig {
 	var cfg Config = config.Get()
 
 	viper := cfg.Viper()
 
-	tlsConfig := &serverAPIClient.TLSConfig{
+	return &serverAPIClient.TLSConfig{
 		CertFile:       viper.GetString("server.http.clients.server.cert_file"),
 		KeyFile:        viper.GetString("server.http.clients.server.key_file"),
 		ServerCertFile: viper.GetString("server.http.clients.server.server_cert_file"),
 	}
+}
 
-	return serverAPIClient.NewWithAutoTLS(viper.GetString("server.http.advertise"), tlsConfig, false)
+func readLeaderAddr() (string, error) {
+	var cfg Config = config.Get()
+
+	viper := cfg.Viper()
+
+	tlsConfig := clientTLSConfig()
+
+	client := serverAPIClient.NewWithAutoTLS(viper.GetString("server.http.advertise"), tlsConfig, false)
+
+	addr, ok, err := client.RaftKVGet(fiber.LeaderAPIAddrKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to get leader API address: %w", err)
+	}
+
+	if !ok {
+		return "", errLeaderAddrNotFound
+	}
+
+	return addr, nil
+}
+
+func getServerAPIClient() ServerAPIClient {
+	tlsConfig := clientTLSConfig()
+	addr, err := readLeaderAddr()
+
+	cobra.CheckErr(err)
+
+	return serverAPIClient.NewWithAutoTLS(addr, tlsConfig, false)
 }
