c79228881f0b0e14ae80e36893ada67478074029
Pavel Sapezhka <me@weastur.com>
2025-04-02 16:19:39 +0300

    test: fix version test

--- internal/utils/version_test.go
+++ internal/utils/version_test.go
@@ -9,8 +9,7 @@ import (
 func TestAppVersion(t *testing.T) {
 	t.Parallel()
 
-	version = "v1.0.0" // Set the version to a known value for testing
-	expected := "v1.0.0"
+	expected := version
 	actual := AppVersion()
 
 	assert.Equal(t, expected, actual, "expected version to be %s, but got %s", expected, actual)
